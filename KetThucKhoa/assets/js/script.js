fdaef62cd60f3e569c5078e0ac3f268ee62dac75
CulesBao <baoduong1709@gmail.com>
2024-04-02 13:09:42 +0700

    Complete main html responsive not js 2

---
+++ KetThucKhoa/assets/js/script.js
@@ -0,0 +1,34 @@
+let list = [
+    [
+        {
+            category: "Todo",
+            title: "title",
+            content: "This is content area. You can write your task in here",
+            dateTime: "dd//mm/yyyy"
+        }
+    ],
+    [
+        {
+            category: "Doing",
+            title: "title",
+            content: "This is content area. You can write your task in here",
+            dateTime: "dd//mm/yyyy"
+        }
+    ],
+    [
+        {
+            category: "Completed",
+            title: "title",
+            content: "This is content area. You can write your task in here",
+            dateTime: "dd//mm/yyyy"
+        }
+    ],
+    [
+        {
+            category: "Blocked",
+            title: "title",
+            content: "This is content area. You can write your task in here",
+            dateTime: "dd//mm/yyyy"
+        }
+    ]
+]
