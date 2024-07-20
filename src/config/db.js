a5710740aea65d877819723d852f944fd1ecee44
CulesBao <baocules17@gmail.com>
2024-07-20 10:13:49 +0700

    Change password mysql

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: 'localhost',
+    user: 'root',
+    password: '123456Aa',
+    database: 'users',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
