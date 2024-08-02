cadc528d654f2faec1bbaf99a43eb5f90f7e7d93
CulesBao <baocules17@gmail.com>
2024-08-02 17:46:50 +0700

    Create dockerfile and docker compose

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '123456Aa',
-    database: 'users',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
