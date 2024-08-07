1f40a38539ff822fc5f163dc3c0403aa2c03f769
CulesBao <baocules17@gmail.com>
2024-08-07 16:59:28 +0700

    change port #3

--- src/app.js
+++ src/app.js
@@ -11,8 +11,9 @@ app.use('/auth', router);
 app.use('', router)
 app.use('/api', router);
 
-app.listen(8000 , () => {
+app.listen(3000 , () => {
     console.log(`Server is running on http://localhost:3000`);
 })
 
 export default app;
+// ssh -i key-ssh ubuntu@ec2-34-227-105-16.compute-1.amazonaws.com
